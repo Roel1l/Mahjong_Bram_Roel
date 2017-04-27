ce0cccfa6b8a4a0c9a723c578096aaa3a5d220a6
Roel1l <raa.guerand@student.avans.nl>
2017-04-27 13:39:40 +0200

    lelzies with the good guys
    dont forget to eat your vegetables boys

--- mahjong/src/app/app.component.ts
+++ mahjong/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,33 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
+  constructor(private heroService: HeroService) { 
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  private onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
 
--- mahjong/src/app/app.module.ts
+++ mahjong/src/app/app.module.ts
@@ -2,21 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 import { AppComponent } from './app.component';
-import { LobbyControllerComponent } from './lobby-controller/lobby-controller.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LobbyControllerComponent
+  AppComponent,
+  HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

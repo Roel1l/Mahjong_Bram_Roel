01e806746074f8fc44f4c51d51ab2d10b9359e25
Roel1l <raa.guerand@student.avans.nl>
2017-04-29 15:41:50 +0200

    memerguyboy

--- mahjong/src/app/app.component.ts
+++ mahjong/src/app/app.component.ts
@@ -1,35 +1,16 @@
 import { Component } from '@angular/core';
-import { HeroDetailComponent } from './hero-detail.component';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+      `,
+    styleUrls: [ './app.component.css' ]
 })
-
-export class AppComponent implements OnInit {
-  title = 'app works!';
-  heroes: Hero[];
-
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { 
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  private getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  private onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+export class AppComponent {
+    title = 'Tour of Heroes';
 }
-
-
--- mahjong/src/app/hero-detail.component.ts
+++ mahjong/src/app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
@@ -11,10 +16,34 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+      <button (click)="goBack()">Back</button>
+      <button (click)="save()">Save</button>
     </div>
-  `
+  `,
+  styleUrls: ['./hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
-    @Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
 }

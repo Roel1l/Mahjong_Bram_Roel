2a7c1e467a04eb418d2598fa2b5f1472d99d76dd
bbrouwer <b.brouwer@student.avans.nl>
2017-05-02 13:37:48 +0200

    Added navigation links

--- mahjong/src/app/app-routing.module.ts
+++ mahjong/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { GamesComponent }      from './games/games.component';
 import { GameDetailComponent }  from './game-detail/game-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/games', pathMatch: 'full' },
+  // { path: '', redirectTo: '/games', pathMatch: 'full' },
   { path: 'games',  component: GamesComponent },
-  { path: 'games/:id', component: GameDetailComponent },
+  { path: 'games/new', component: GamesComponent},
+  { path: 'games/:id', component: GameDetailComponent }
+ 
 ];
 
 @NgModule({

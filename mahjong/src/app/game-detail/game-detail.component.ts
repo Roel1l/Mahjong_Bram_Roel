070f564307e3531f7dc40f3b78a57bc172eadf1c
bbrouwer <b.brouwer@student.avans.nl>
2017-05-04 13:54:07 +0200

    delete game

--- mahjong/src/app/game-detail/game-detail.component.ts
+++ mahjong/src/app/game-detail/game-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
+import { Router } from '@angular/router';
 
 //Models
 import { Game } from '../models/game';
@@ -21,8 +22,9 @@ import { UserService } from '../services/user.service';
 export class GameDetailComponent implements OnInit {
 
   @Input() game: Game;
-
+  currentUser: User;
   constructor(
+     private router: Router,
     private gameService: GameService,
     private userService: UserService,
     private route: ActivatedRoute,
@@ -33,12 +35,14 @@ export class GameDetailComponent implements OnInit {
      this.route.params
       .switchMap((params: Params) => this.gameService.getGame(params['id']))
       .subscribe(game => this.game = game);
+      this.currentUser = this.userService.getUser();
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  //TODO hoort dit niet te redirecten?//refreshen
   joinGame(): void {
     var user: User = this.userService.getUser();
     this.route.params.subscribe((params: Params) => {
@@ -51,5 +55,11 @@ export class GameDetailComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
         this.gameService.leaveGame(params['id'], user);
     });
+  }
+
+  deleteGame(): void { 
+    this.gameService.delete(this.game._id, this.currentUser).then(() => {
+             this.router.navigate(['/games']);  
+      });
   }
 }

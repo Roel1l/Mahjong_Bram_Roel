29b20a08ec21077a9aa60de08664f6c5315de3bf
bbrouwer <b.brouwer@student.avans.nl>
2017-05-10 13:49:16 +0200

    giv eme emomen spls

--- mahjong/src/app/play-game/play-game.component.ts
+++ mahjong/src/app/play-game/play-game.component.ts
@@ -8,12 +8,13 @@ import { Router } from '@angular/router';
 //Models
 import { Game } from '../models/game';
 import { User } from '../models/user';
+import { Tile } from '../models/tile';
 
 //Services
 import { GameService } from '../services/game.service';
 import { UserService } from '../services/user.service';
 import { UserDependendComponent } from "app/core/UserDependend.base";
-
+import { TileService } from '../services/tile.service';
 @Component({
   selector: 'app-play-game',
   templateUrl: './play-game.component.html',
@@ -28,7 +29,8 @@ export class PlayGameComponent extends UserDependendComponent implements OnInit 
     private gameService: GameService,
     private route: ActivatedRoute,
     private location: Location,
-    userService: UserService
+    userService: UserService,
+    private tileService: TileService
   ) {
     super(userService);
   }
@@ -40,6 +42,14 @@ export class PlayGameComponent extends UserDependendComponent implements OnInit 
       .subscribe(game => {
         this.game = game;
       });
+  }
+
+  getTiles(){
+    this.tileService.getTylesByGame(this.game._id,false).then(
+      function(response){
+        console.log(response);
+      }
+    )
   }
 
 }

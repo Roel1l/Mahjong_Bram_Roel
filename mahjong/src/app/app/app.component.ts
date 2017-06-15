c247ba3818b86b95abd7706de9e7672a76c10b2a
Roel1l <raa.guerand@student.avans.nl>
2017-06-16 01:58:28 +0200

    location fix

--- mahjong/src/app/app/app.component.ts
+++ mahjong/src/app/app/app.component.ts
@@ -13,7 +13,7 @@ import { ToastService } from '../services/toast.service';
 })
 export class AppComponent extends UserDependendComponent implements OnInit {
   
-  authUrl: string =  'https://mahjongmayhem.herokuapp.com/auth/avans/?callbackUrl=https://roel1l.github.io/Mahjong_Bram_Roel/';
+ authUrl: string =  'https://mahjongmayhem.herokuapp.com/auth/avans/?callbackUrl=' + window.location.origin;
   
   constructor(
     private router: Router,

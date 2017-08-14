09583d824ba538c926e40504c5c65fa2e9d0f14e
bbrouwer <b.brouwer@student.avans.nl>
2017-08-14 14:59:07 +0200

    added match history component ofzo

--- mahjong/src/app/app-routing.module.ts
+++ mahjong/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { GamesComponent }      from './games/games.component';
 import { GameDetailComponent }  from './game-detail/game-detail.component';
 import { NewGameComponent } from './new-game/new-game.component';
 import { PlayGameComponent } from "app/play-game/play-game.component";
+import { MatchHistoryComponent } from "app/match-history/match-history.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/games', pathMatch: 'full' },
   { path: 'games',  component: GamesComponent },
   { path: 'games/new', component: NewGameComponent },
   { path: 'games/:id', component: GameDetailComponent },
-  { path: 'games/:id/play', component: PlayGameComponent }
+  { path: 'games/:id/play', component: PlayGameComponent },
+  { path: 'games/:id/history', component: MatchHistoryComponent }
 ];
 
 @NgModule({
--- mahjong/src/app/match-history/match-history.component.ts
+++ mahjong/src/app/match-history/match-history.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+import { Router } from '@angular/router'
+
+//Models
+import { Game } from '../models/game';
+import { User } from '../models/user';
+import { Tile } from '../models/tile';
+
+//Services
+import { GameService } from '../services/game.service';
+import { UserService } from '../services/user.service';
+import { UserDependendComponent } from "app/core/UserDependend.base";
+import { TileService } from '../services/tile.service';
+import { ToastService } from "app/services/toast.service";
+import { SocketService } from "app/services/socket.service";
 
 @Component({
   selector: 'app-match-history',
   templateUrl: './match-history.component.html',
   styleUrls: ['./match-history.component.scss']
 })
-export class MatchHistoryComponent implements OnInit {
+export class MatchHistoryComponent extends UserDependendComponent implements OnInit {
 
-  constructor() { }
+ @Input() game: Game;
+  tiles: Tile[]
+
+  constructor(
+    private router: Router,
+    private gameService: GameService,
+    private route: ActivatedRoute,
+    private location: Location,
+    userService: UserService,
+    private tileService: TileService,
+    private socketService: SocketService,
+    public toastService: ToastService
+  ) {
+    super(userService);
+  }
 
   ngOnInit() {
+    super.ngOnInit();
+    this.route.params
+      .switchMap((params: Params) => this.gameService.getGame(params['id']))
+      .subscribe(game => {
+        this.game = game;
+        this.getTiles();
+      });
+  }
+
+    getTiles() {
+    var self = this;
+    self.tiles = [];
+    this.tileService.getTilesByGame(this.game._id, false).then(
+      function (response) {
+        self.tiles = response;
+      }
+    )
   }
 
 }
